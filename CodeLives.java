f77245f63bf459b7343a98e8833d4227328f4f2c
DESKTOP-LF2PVSQ\Erick Ciudad <ErickCiudad1@gmail.com>
2017-11-16 23:04:01 -0700

    comment on what happened

--- CodeLives.java
+++ CodeLives.java
@@ -4,7 +4,12 @@ public class CodeLives {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is going to GitHub");
+
+		
+		for (int i = 0; i < 11; i++) {
+			System.out.println(i);
+
+		}
 		
 	}
 
